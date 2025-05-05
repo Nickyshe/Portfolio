0d1106032359e010faf65bd6195fa4a8d2827ae3
Nickyshe <judypearls200@gmail.com>
2025-05-05 19:05:50 +0100

    fix image error

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -192,7 +192,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/pfp.jpg"
+              src="../../images/me.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
